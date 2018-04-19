38cbfcb8dbb7042f3f74852e238b95d117d4844a
Edwin Gerard <edwin.gerard1@gmail.com>
2018-04-19 10:36:06 +0200

    Ajout de la récursivité et de la suppression des dossiers

--- index.php
+++ index.php
@@ -1,47 +1,64 @@
-<?php
-if (!empty($_POST['delete'])){
-
-    $fileName = $_POST['filedir'].$_POST['filename'];
-    if (file_exists($fileName)) {
-        unlink($fileName);
-    }
-
-}
-if (!empty($_POST['envoyer'])){
-    $filePath = $_POST['chemin'];
-    $newContend = $_POST['contend'];
-    $file = fopen($filePath, "w");
-    fwrite($file, $newContend);
-    fclose($file);
-}
-?>
 <?php include('inc/head.php'); ?>
-
 <?php
-$path = 'files';
-$allDir = scandir($path);
-for($i = 2; $i < count($allDir); $i++) {?>
-    <?php
-    $allFiles = scandir($path . '/' . $allDir[$i]);
-    for ($j = 2; $j < count($allFiles); $j++) {
-        ?>
-        <div class="caption">
-            <a href="<?= 'files/'.$allDir[ $i ].'/'.$allFiles[$j]; ?>">files/<?= $allDir[ $i ]; ?>/<?= $allFiles[$j]; ?></a>
-            <form action="edit.php" method="post">
-                <input type="hidden" name="filename" value="<?= $path.'/'.$allDir[$i].'/'.$allFiles[$j]; ?>">
-                <input type="submit" name="modify" value="Modify">
-            </form>
-            <form action="" method="post">
-                <input type="hidden" name="filename" value="<?= $allFiles[$j]; ?>">
-                <input type="hidden" name="filedir" value="<?= $path . '/' . $allDir[$i] . '/'; ?>">
-                <input type="submit" name="delete" value="Delete">
-            </form>
-        </div>
-        <?php
+
+if(!empty($_GET["name"])) {
+    if(!is_dir($_GET["name"])) {
+        unlink($_GET["name"]);
+    } else {
+        $path=($_GET["name"]);
+        $handle = opendir($path);
+        while (false !== ($file = readdir($handle))) {
+            if ($file != '.' and $file != '..') {
+                $fullpath = $path ."/". $file;
+                if (is_dir($fullpath)) rmdir_recurse($fullpath); else unlink($fullpath);
+            }
+        }
+        closedir($handle);
+        rmdir($path);
     }
-    ?>
-    <?php
 }
+/**
+ * @param $dir
+ */
+function mkmap($dir) {
+    echo "<ul>"; $folder = opendir ($dir);
+    while ($file = readdir ($folder)) {
+        if ($file != "." && $file != "..") {
+            $extension= pathinfo($file, PATHINFO_EXTENSION);
+            $pathfile = $dir.'/'.$file;
+            if ($extension =="txt" || $extension =="html") {
+                echo "<li>
+                        <p>
+                            <a class=\"green\" type=\"submit\" method=\"GET\" href=\"delete.php?f=$pathfile\">$file</a>
+                            <a type=\"submit\" method=\"GET\" href=\"index.php?name=$pathfile\">Supprimer</a>
+                        </p>
+                      </li>";
+            } elseif ($extension == "jpg") {
+                echo "<li>
+                          <a class=\"green\" href=$pathfile>$file</a>
+                          <a type=\"submit\" method=\"GET\" href=\"index.php?name=$pathfile\">Supprimer
+                      </li>";
+            } else {
+                echo "<li>
+                        <p>
+                            $file<a type=\"submit\" method=\"GET\" href=\"index.php?name=$pathfile\">Supprimer Dossier</a>
+                        </p>
+                      </li>";
+            }
+            if(filetype($pathfile) == 'dir') {
+                mkmap($pathfile);
+            }
+        }
+    }
+    closedir ($folder);
+    echo "</ul>";
+}
+mkmap('files');
 ?>
+
+
+
+
+
 
 <?php include('inc/foot.php'); ?>
